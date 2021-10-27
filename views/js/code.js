8043ec52ec4323786f871b61077b7e5a11a11d2b
Gircsi117 <gilianerik117@gmail.com>
2021-10-27 22:38:25 +0200

    Responz menu befejezése

--- views/js/code.js
+++ views/js/code.js
@@ -1,5 +1,6 @@
 var menu = document.getElementById("menu");
 var opener = document.getElementById("opener");
+var menu_icons = document.getElementsByClassName("menu_icon")
 
 
 document.addEventListener("scroll", ()=>{
@@ -15,9 +16,17 @@ opener.addEventListener("click", ()=>{
     if (!menu.classList.contains("lathato")) {
         menu.classList.add("lathato")
         menu.classList.remove("nem_lathato")
+
+        menu_icons[1].style.display = "none";
+        menu_icons[0].classList.add("fordul_jobb")
+        menu_icons[2].classList.add("fordul_bal")
     }
     else{
         menu.classList.remove("lathato")
         menu.classList.add("nem_lathato")
+
+        menu_icons[1].style.display = "block";
+        menu_icons[0].classList.remove("fordul_jobb")
+        menu_icons[2].classList.remove("fordul_bal")
     }
 })
